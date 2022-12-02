2a32247d530e89a00d7eb556f59b1376f1eda6da
LeSnake <dev.lesnake@posteo.de>
2022-12-02 22:27:16 +0100

    added d2 to chooser

--- src/y2022/mod.rs
+++ src/y2022/mod.rs
@@ -1,11 +1,13 @@
 use anyhow::{anyhow, Result};
 
 pub mod d01;
+pub mod d02;
 
 pub fn run(day: u8) -> Result<()> {
 	match day {
-		1 => d01::main(),
+		1 => d01::main()?,
+		2 => d02::main(),
 		d => Err(anyhow!("Invalid day: {}", d))?,
-	}?;
+	};
 	Ok(())
 }
